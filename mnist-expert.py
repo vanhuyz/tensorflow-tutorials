ec27d50679a3c7a60678056b55729f81eb4c1bb6
Van Huy <huyvpq@gmail.com>
2016-03-24 02:46:46 +0900

    Remove unneeded code

--- mnist-expert.py
+++ mnist-expert.py
@@ -7,12 +7,6 @@ sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-
-sess.run(tf.initialize_all_variables())
-
-y = tf.nn.softmax(tf.matmul(x,W) + b)
 
 # Weight Initialization
 def weight_variable(shape):
